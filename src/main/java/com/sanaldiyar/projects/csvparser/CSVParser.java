6895aa0398f22908ab336c5fc5b02b93f035e92d
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2012-06-22 18:52:26 +0300

    project start

---
+++ src/main/java/com/sanaldiyar/projects/csvparser/CSVParser.java
@@ -0,0 +1,13 @@
+package com.sanaldiyar.projects.csvparser;
+
+/**
+ * Hello world!
+ *
+ */
+public class CSVParser 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
